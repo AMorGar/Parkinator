247baef54f8046e2a99f25510cee82b9001a84a9
AlejandroMorenoGarrido <amorgar539@g.educaand.es>
2023-01-18 20:02:56 +0100

    primer commit

---
+++ src/main/java/com/Alejandro/Amanda/Gonzalo/Parkinator/ParkinatorApplication.java
@@ -0,0 +1,13 @@
+package com.Alejandro.Amanda.Gonzalo.Parkinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Alejandro/Amanda/Gonzalo/Parkinator/ParkinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alejandro.Amanda.Gonzalo.Parkinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
