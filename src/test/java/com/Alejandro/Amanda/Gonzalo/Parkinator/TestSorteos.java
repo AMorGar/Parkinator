3ce3db71ce1562b03fff5de1d745664fa92a7c1a
AlejandroMorenoGarrido <amorgar539@g.educaand.es>
2023-02-01 21:10:08 +0100

    FixTestSorteos

--- src/test/java/com/Alejandro/Amanda/Gonzalo/Parkinator/TestSorteos.java
+++ src/test/java/com/Alejandro/Amanda/Gonzalo/Parkinator/TestSorteos.java
@@ -9,7 +9,7 @@ import com.Alejandro.Amanda.Gonzalo.Parkinator.Sorteos.Sorteos;
 public class TestSorteos {
     
     @Test
-    public void check_user_name() {
+    public void check_user_Estado() {
         // Arrange
         Sorteos Primero = new Sorteos("Activo", "Primer Sorteo", "01/02/2023");
         String expected = "Activo";
@@ -22,6 +22,33 @@ public class TestSorteos {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void check_user_Fecha() {
+        // Arrange
+        Sorteos Primero = new Sorteos("Activo", "Primer Sorteo", "01/02/2023");
+        String expected = "01/02/2023";
+
+        // Act
+        String actual = Primero.getFecha();
+
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void check_user_name() {
+        // Arrange
+        Sorteos Primero = new Sorteos("Activo", "Primer Sorteo", "01/02/2023");
+        String expected = "Activo";
+
+        // Act
+        String actual = Primero.getEstado();
+
+
+        // Assert
+        assertEquals(expected, actual);
+    }
     
 }
 
