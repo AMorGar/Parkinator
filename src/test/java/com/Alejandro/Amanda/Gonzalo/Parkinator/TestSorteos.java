3c38b30fb5b59c055812240a14fc7692a0dfe2cb
gruiazu <gruiazu@g.educaand.es>
2023-02-08 20:43:40 +0100

    CambiosIdSorteos

--- src/test/java/com/Alejandro/Amanda/Gonzalo/Parkinator/TestSorteos.java
+++ src/test/java/com/Alejandro/Amanda/Gonzalo/Parkinator/TestSorteos.java
@@ -9,6 +9,20 @@ import com.Alejandro.Amanda.Gonzalo.Parkinator.Sorteos.Sorteos;
 public class TestSorteos {
     
     @Test
+    public void check_Sorteos_idSorteo() {
+        // Arrange
+        Sorteos Primero = new Sorteos("Activo", "Primer Sorteo", "01/02/2023");
+        String expected = "123456789";
+
+        // Act
+        String actual = Primero.getidSorteo();
+
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void check_Sorteos_Estado() {
         // Arrange
         Sorteos Primero = new Sorteos("Activo", "Primer Sorteo", "01/02/2023");
