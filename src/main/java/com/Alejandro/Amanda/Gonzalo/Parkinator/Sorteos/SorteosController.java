1281ddca53c0d39a3bea1973694530181bbadf38
gruiazu <gruiazu@g.educaand.es>
2023-02-08 21:57:00 +0100

    FixSorteosController

--- src/main/java/com/Alejandro/Amanda/Gonzalo/Parkinator/Sorteos/SorteosController.java
+++ src/main/java/com/Alejandro/Amanda/Gonzalo/Parkinator/Sorteos/SorteosController.java
@@ -1,5 +1,6 @@
 package com.Alejandro.Amanda.Gonzalo.Parkinator.Sorteos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SorteosController {
     
-    private SorteosRepository repository = new SorteosRepository();
+    @Autowired
+    private SorteosRepository repository;
 
     @GetMapping("/Sorteos")
     public String displaySorteos(Model model) {
+        model.addAttribute("Sorteos", repository.getAll());
         return "ListaSorteos";
     }
 }
