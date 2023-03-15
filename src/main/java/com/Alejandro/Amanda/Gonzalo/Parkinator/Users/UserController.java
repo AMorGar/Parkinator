2b945ae85273a86d81a7809b717d48c6aac0b45d
AlejandroMorenoGarrido <amorgar539@g.educaand.es>
2023-03-15 19:44:05 +0100

    Cambios formulario(no los guarda bien)

--- src/main/java/com/Alejandro/Amanda/Gonzalo/Parkinator/Users/UserController.java
+++ src/main/java/com/Alejandro/Amanda/Gonzalo/Parkinator/Users/UserController.java
@@ -28,7 +28,9 @@ public class UserController {
 
     @PostMapping("/CreateUser")
     public String createUser(@ModelAttribute  UserDao userDao ){
-    return "redirect:/users"; 
+
+        
+    return "redirect:/Users"; 
     }
 
     
